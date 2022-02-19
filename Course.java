5ccba44023f25be1df55db8d19e6d532412d6d0b
Aryamaniac <aryamansingh115@gmail.com>
2022-02-19 12:01:09 -0500

    started testing, added more constructors

--- Course.java
+++ Course.java
@@ -1,7 +1,15 @@
+
 public class Course {
     String name;
     String prefix;
     int coursenum;
     int CID;
     int hours;
+
+    public Course(String pr, int cn, int ci, int h) {
+        prefix = pr;
+        coursenum = cn;
+        CID = ci;
+        hours = h;
+    }
 }
