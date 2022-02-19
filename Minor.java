8136435ae8048f54328927c5372a0f6c25dff676
Aryamaniac <aryamansingh115@gmail.com>
2022-02-19 10:48:07 -0500

    matching logic

--- Minor.java
+++ Minor.java
@@ -1,9 +1,26 @@
+import java.util.*;
+
 public class Minor {
     int totalHours;
     int remainingHours;
+    int reqHoursMin;
+    int reqHoursMax;
+    int optHoursMin;
+    int optHoursMax;
     String name;
-    Course required[];
-    Course optionals[];
+    ArrayList<Course> required;
+    ArrayList<Course> optionals;
 
-    
+    public void removeTaken(ArrayList<Course> taken) {
+        for (Course c : taken) {
+            if (required.indexOf(c) == -1 && optionals.indexOf(c) == -1) {
+                taken.remove(c);
+            }
+        }
+
+        for (Course c : taken) {
+            
+        }
+    }
+
 }
