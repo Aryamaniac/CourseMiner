193760938529d435d036085353d90c9bdb066f0c
Chris Hunter <crhjr13@gmail.com>
2022-02-19 09:18:02 -0500

    Test to make sure branching works

--- Minor.java
+++ Minor.java
@@ -1,9 +1,22 @@
 public class Minor {
+   
     int totalHours;
     int remainingHours;
     String name;
-    Course required[];
-    Course optionals[];
+    Course required[] = new Course[10];
+    Course optionals[] = new Course[10];
+    int requiredSize = 0;
+    int optionalSize = 0;
+    int minRequiredHours;
+    int maxRequiredHours;
+    int optionalHours; 
+
+    public void requiredAppend(Course c){
+        //Insert code to expand array if needed?
+        required[requiredSize] = c;
+        requiredSize++;
+    } // requiredAppend
+    
 
     
 }
