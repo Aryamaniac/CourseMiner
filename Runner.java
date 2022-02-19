6b5a31f193a15353fa25b3073a856f6728648631
Aryamaniac <aryamansingh115@gmail.com>
2022-02-19 12:01:54 -0500

    testing

---
+++ Runner.java
@@ -0,0 +1,7 @@
+public class Runner {
+    public static void main(String[] args) {
+        Course c1 = new Course("BCMB", 3100, 6985, 3);
+        Course c2 = new Course("CBIO", 3400, 4830, 4);
+
+    }
+}
